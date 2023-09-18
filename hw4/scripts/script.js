82d30fec036627c836a8e62a46274473eb084db2
DavidBakalov21 <davidbakalov33@gmail.com>
2023-09-18 18:36:35 +0300

    hw4

---
+++ hw4/scripts/script.js
@@ -0,0 +1,9 @@
+function DropDown(){
+    var DropDown=document.getElementById('Drop').style.display = "flex";
+
+}
+
+function DropUp(){
+    var DropDown=document.getElementById('Drop').style.display = "none";
+
+}
